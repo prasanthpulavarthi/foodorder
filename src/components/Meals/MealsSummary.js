6dc9ef34e96c6b8a6c296ebe264aa3c9d8a8f479
prasanth19 <saiprasanth.neosoft@gmail.com>
2022-06-27 10:34:32 +0530

    food order app

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+const MealsSummary=()=>{
+    return(
+    <section className={classes.summary}>
+        <h2>Delicious Food,Delivered To You</h2>
+        <p>
+            Choose your favourite meal from our broad selsction of available meals and enjoy a delicious lunch or dinner at home
+
+        </p>
+        <p>
+            All our meals are cooked with high-quality ingredients,just-in-time and of course by experenced chefs!
+        </p>
+    </section>
+    )
+
+
+
+}
+
+export default MealsSummary;
---
+++ src/store/cart-context.js
@@ -0,0 +1,8 @@
+import React, { createContext } from "react";
+const CartContext=React.createContext({
+    items:[],
+    totalAmount:0,
+    addItem:(item)=>{},
+    removeItem:(id)=>{}
+})
+export default CartContext;
